0de228a72341d84e1602acfec54fb2f80522b47f
Mirniivoin21 <superasus46@yandex.ru>
2017-06-20 01:06:02 +0800

    Upd #2

--- BurAz/ViewModel/MainViewModel.cs
+++ BurAz/ViewModel/MainViewModel.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Windows;
+using System.Windows.Threading;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace BurAz.ViewModel
 {
@@ -6,6 +10,39 @@ namespace BurAz.ViewModel
     {
         public MainViewModel()
         {
+            _dispTimerCounter = time;
+            _dispTimer = new DispatcherTimer();
+            _dispTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            _dispTimer.Tick += new EventHandler(dispTimer_Tick);
         }
+
+        private void dispTimer_Tick(object sender, EventArgs e)
+        {
+            ABCTimer = (_dispTimerCounter--).ToString();
+            if (_dispTimerCounter == 0)
+            {
+                _dispTimerCounter = time;
+            }
+        }
+
+        private const int time = 10;
+
+        DispatcherTimer _dispTimer;
+        int _dispTimerCounter;
+
+        private string _abcTimer;
+        public string ABCTimer
+        {
+            get { return _abcTimer; }
+            set { _abcTimer = value; RaisePropertyChanged(() => ABCTimer); }
+        }
+
+        public RelayCommand ABCStart => new RelayCommand(() => _dispTimer.Start());
+
+        public string ABCLetter { get; set; }
+
+        public string ABCTrue { get; set; }
+
+        public string ABCFalse { get; set; }
     }
 }
